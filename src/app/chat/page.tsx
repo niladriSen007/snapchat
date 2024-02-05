62ae7b3ff4552780ff609c341fe947c4eead16a7
Niladri <niladrisengit12@gmail.com>
2024-02-05 22:55:37 +0530

    day-03

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,6 +1,19 @@
+import ChatCamera from "@/components/chat/ChatCamera"
+import Image from "next/image"
+
 const page = () => {
   return (
-    <div>page</div>
+    <main className='flex-grow bg-sigMain items-center flex px-2'>
+			<div
+				className='bg-chat bg-right-bottom
+				rounded-3xl w-full h-screen flex items-center justify-center px-6'
+			>
+				<ChatCamera />
+				<div className='hidden lg:block'>
+					<Image src={"/snapemoji.png"} width={500} height={600} alt='Snap avatar' />
+				</div>
+			</div>
+		</main>
   )
 }
 export default page
